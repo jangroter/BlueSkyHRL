2b8545009f97327633e1cc7c66d1336c9006d874
jangroter <47716241+jangroter@users.noreply.github.com>
2025-05-12 14:21:35 +0200

    fix indentation

--- plugins/Sink.py
+++ plugins/Sink.py
@@ -68,35 +68,35 @@ class Sink(core.Entity):
         traf.control_mode[-n:] = np.zeros(n)
 
     def _set_terminal_conditions(self):
-            """
-            Creates the terminal conditions surrounding the FAF to ensure correct approach angle
+        """
+        Creates the terminal conditions surrounding the FAF to ensure correct approach angle
 
-            If render mode is set to 'human' also already creates the required elements for plotting 
-            these terminal conditions in the rendering window
-            """
-            for rwy in self.runway:
-                num_points = 36 # number of straight line segments that make up the circle
+        If render mode is set to 'human' also already creates the required elements for plotting 
+        these terminal conditions in the rendering window
+        """
+        for rwy in self.runway:
+            num_points = 36 # number of straight line segments that make up the circle
 
-                faf_lat, faf_lon = fn.get_point_at_distance(RUNWAYS_SCHIPHOL_FAF[rwy]['lat'],
-                                                            RUNWAYS_SCHIPHOL_FAF[rwy]['lon'],
-                                                            FAF_DISTANCE,
-                                                            RUNWAYS_SCHIPHOL_FAF[rwy]['track']-180)
-                
-                # Compute bounds for the merge angles from FAF
-                cw_bound = ((RUNWAYS_SCHIPHOL_FAF[rwy]['track']-180+ 360)%360) + (IAF_ANGLE/2)
-                ccw_bound = ((RUNWAYS_SCHIPHOL_FAF[rwy]['track']-180+ 360)%360) - (IAF_ANGLE/2)
-
-                angles = np.linspace(cw_bound,ccw_bound,num_points)
-                lat_iaf, lon_iaf = fn.get_point_at_distance(faf_lat, faf_lon, IAF_DISTANCE, angles)
-
-                command = f'POLYLINE SINK{rwy}'
-                for i in range(0,len(lat_iaf)):
-                    command += ' '+str(lat_iaf[i])+' '
-                    command += str(lon_iaf[i])
-                stack.stack(command)
+            faf_lat, faf_lon = fn.get_point_at_distance(RUNWAYS_SCHIPHOL_FAF[rwy]['lat'],
+                                                        RUNWAYS_SCHIPHOL_FAF[rwy]['lon'],
+                                                        FAF_DISTANCE,
+                                                        RUNWAYS_SCHIPHOL_FAF[rwy]['track']-180)
             
-                stack.stack(f'POLYLINE RESTRICT{rwy} {lat_iaf[0]} {lon_iaf[0]} {faf_lat} {faf_lon} {lat_iaf[-1]} {lon_iaf[-1]}')
-                stack.stack(f'COLOR RESTRICT{rwy} red')
+            # Compute bounds for the merge angles from FAF
+            cw_bound = ((RUNWAYS_SCHIPHOL_FAF[rwy]['track']-180+ 360)%360) + (IAF_ANGLE/2)
+            ccw_bound = ((RUNWAYS_SCHIPHOL_FAF[rwy]['track']-180+ 360)%360) - (IAF_ANGLE/2)
+
+            angles = np.linspace(cw_bound,ccw_bound,num_points)
+            lat_iaf, lon_iaf = fn.get_point_at_distance(faf_lat, faf_lon, IAF_DISTANCE, angles)
+
+            command = f'POLYLINE SINK{rwy}'
+            for i in range(0,len(lat_iaf)):
+                command += ' '+str(lat_iaf[i])+' '
+                command += str(lon_iaf[i])
+            stack.stack(command)
+        
+            stack.stack(f'POLYLINE RESTRICT{rwy} {lat_iaf[0]} {lon_iaf[0]} {faf_lat} {faf_lon} {lat_iaf[-1]} {lon_iaf[-1]}')
+            stack.stack(f'COLOR RESTRICT{rwy} red')
 
     def _get_terminated(self, id):
         """
